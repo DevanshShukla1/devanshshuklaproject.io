fcc0341dfd3e221cbc57527895ca3f445817fc7e
DevanshShukla1 <78690713+DevanshShukla1@users.noreply.github.com>
2023-01-30 20:04:22 +0530

    Add files via upload

---
+++ jobportal1/footer.php
@@ -0,0 +1,9 @@
+<div class="footer_bottom">	
+  <div class="container">
+   
+  	<div class="clearfix"> </div>
+	<div class="copy">
+		<p>Copyright © 2022. All Rights Reserved<a href="index.php" target="_blank">Approved</a> </p>
+	</div>
+  </div>
+</div>
